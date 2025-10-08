68c5fb94913ce2a923c0888ad297a5c64ac06334
gabotii <gtorrejonaguilera@gmail.com>
2025-10-08 10:05:45 -0300

    commit smoke tests

--- tests/test_smoketest.py
+++ tests/test_smoketest.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-yes
+
 class TestSmoketest():
   def setup_method(self, method):
     self.driver = webdriver.Firefox()
